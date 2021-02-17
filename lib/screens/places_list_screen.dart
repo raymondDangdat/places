6a780d1cbd907dfed490f6c4daba2eb1a0805b1e
Dangdat <raymonddangdat@gmail.com>
2021-02-17 21:38:27 +0100

    Implemented UI for adding a place

--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
+import './add_place_screen.dart';
+
 class PlacesListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Your Places"),
+        centerTitle: true,
         actions: [
-          IconButton(icon: Icon(Icons.add), onPressed: null)
+          IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () {
+                Navigator.of(context).pushNamed(AddPlace.routeName);
+              })
         ],
       ),
       body: Center(
